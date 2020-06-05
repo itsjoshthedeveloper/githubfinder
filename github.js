cffc48d7da96c309291e3000ee18ba775eb85bb2
Josh Chough <joshchough@gmail.com>
2020-06-05 17:47:38 -0400

    Added comments to github.js

--- github.js
+++ github.js
@@ -2,16 +2,21 @@ class GitHub {
   constructor() {
     this.client_id = '477c986d5b2dbd5249af';
     this.client_secret = 'ce27bc52b26be994d8aafae654f763682697bd8b';
+    // Number of returned repos
     this.repos_count = 5;
+    // Repos sort
     this.repos_sort = 'created: asc';
   }
 
+  // Makes http request to get a user
   async getUser(user) {
+    // Gets profile
     const profileResponse = await fetch(
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
     const profile = await profileResponse.json();
 
+    // Gets repos
     const reposResponse = await fetch(
       `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
